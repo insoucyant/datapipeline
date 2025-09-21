26f678ac81a7da55042cafd58d322b41bd96ab8c
Sumit Singh <sumit20@gmail.com>
2025-09-21 09:57:24 +0530

    updated the data insert file

--- api-request/insert_records.py
+++ api-request/insert_records.py
@@ -1,3 +1,44 @@
 from api_request import mock_fetch_data
+import psycopg2 
 
-print(mock_fetch_data())
+def connect_to_db():    
+    print("Connecting to the PostgresSQL database...")
+    try:
+       conn = psycopg2.connect(
+            host="localhost",
+            port=5000,
+            dbname="db",
+            user="db_user",
+            password="db_password"
+        )
+       return conn
+    except psycopg2.Error as e:
+        print(f"Database connection failed: {e}")
+        raise
+
+def create_table(conn):
+    print("Creating table if not exist...")
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE SCHEMA IF NOT EXISTS dev;
+            CREATE TABLE IF NOT EXISTS dev.raw_weather_data (
+                id SERIAL PRIMARY KEY,
+                city TEXT,
+                temperature FLOAT,
+                weather_description TEXT,
+                wind_speed FLOAT,
+                time TIMESTAMP,
+                inserted_at TIMESTAMP DEFAULT NOW(),
+                utc_offset TEXT
+            );
+        """)
+        conn.commit()
+        print("Table was created ")
+    except psycopg2.Error as e:
+        print(f"Failed to create the table: {e}")
+        raise
+
+conn = connect_to_db() 
+create_table(conn)
+
