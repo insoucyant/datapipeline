281f100b5d9d33552a7036517c07adc7d5088be9
Sumit Singh <sumit20@gmail.com>
2025-09-24 13:37:45 +0530

    Changes to airflow orchestrator and airflow_init.sql

---
+++ airflow/dags/orchestrator.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import sys
+
+sys.path.append('/opt/airflow/api-request')
+
+def safe_main_callable():
+    from insert_records import main
+    return main()
+
+default_args = {
+    'start_date':datetime(2025,9,24)
+    'retries':1,
+    'retry_delay':timedelta(minutes=5)}
+
+dag = DAG(
+    dag_id = 'weather-api-orchestrator',
+    default_args=default_args,
+    description = 'A DAG to orchestrate data',
+    schedule=timedelta(minutes=10),
+    catchup=False
+)
+
+with dag:
+    task1 = PythonOperator(
+        task_id='ingest_data_task',
+        python_callable=safe_main_callable
+    )
